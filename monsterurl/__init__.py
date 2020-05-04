3008ba9f8156e93082a5306b706d45894e613049
Aravind Venugopal <venugopal.aravind94@gmail.com>
2020-05-04 16:03:10 -0700

    Added support for animals

--- monsterurl/__init__.py
+++ monsterurl/__init__.py
@@ -6,14 +6,20 @@ ROOT = os.path.abspath(os.path.dirname(__file__))
 ASSETS = os.path.join(ROOT, 'assets')
 ADJECTIVES = os.path.join(ASSETS, 'adjectives')
 MONSTERS = os.path.join(ASSETS, 'monsters')
+ANIMALS = os.path.join(ASSETS, 'animals')
 
-def get_monster():
+def get_monster(monster = False):
     with open(ADJECTIVES) as f:
         adjectives = f.read().splitlines()
     with open(MONSTERS) as f:
         monsters = f.read().splitlines()
+    with open(ANIMALS) as f:
+        animals = f.read().splitlines()
     adjective_1 = capwords(random.choice(adjectives))
     adjective_2 = capwords(random.choice(adjectives))
-    monster = random.choice(monsters)
-    return "%s%s%s" % (adjective_1, adjective_2, monster)
+    if monster:
+        creature = random.choice(monsters)
+    else:
+        creature = random.choice(animals)
+    return "%s%s%s" % (adjective_1, adjective_2, creature)
 
