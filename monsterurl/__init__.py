72fb628c8ef287a45e3e481f2b612c54a20f2fa1
Aravind Venugopal <venugopal.aravind94@gmail.com>
2020-08-31 17:21:52 -0700

    Added Letter support

--- monsterurl/__init__.py
+++ monsterurl/__init__.py
@@ -8,13 +8,19 @@ ADJECTIVES = os.path.join(ASSETS, 'adjectives')
 MONSTERS = os.path.join(ASSETS, 'monsters')
 ANIMALS = os.path.join(ASSETS, 'animals')
 
-def get_monster(monster = False):
+def get_monster(monster = False, letter = None):
     with open(ADJECTIVES) as f:
         adjectives = f.read().splitlines()
     with open(MONSTERS) as f:
         monsters = f.read().splitlines()
     with open(ANIMALS) as f:
         animals = f.read().splitlines()
+    if letter:
+        letter = letter.upper()
+        animals = [a for a in animals if a.startswith(letter)]
+        monsters = [a for a in monsters if a.startswith(letter)]
+        adjectives = [a for a in adjectives if a.startswith(letter.lower())]
+        
     adjective_1 = capwords(random.choice(adjectives))
     adjective_2 = capwords(random.choice(adjectives))
     if monster:
